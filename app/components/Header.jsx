a66f4a120378c50243c4a74e8e303ef7c98c5483
THEYASHGAUR <yashgaur2525@gmail.com>
2025-02-12 20:44:09 -0800

    updated home page & contact page

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,7 @@
-"use client";
+'use client';
 
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
@@ -9,12 +10,19 @@ const Header = () => {
         <img src="logo.png" alt="Logo" className="h-24 w-auto" />
       </div>
       <nav className="hidden md:flex space-x-6 text-gray-700">
-        <a href="#" className="hover:text-blue-500">Home</a>
-        <a href="#" className="hover:text-blue-500">About</a>
-        <a href="#" className="hover:text-blue-500">Services</a>
+        <a href="/" className="hover:text-blue-500">
+          Home
+        </a>
+        <a href="/about" className="hover:text-blue-500">
+          About
+        </a>
+        <a href="/services" className="hover:text-blue-500">
+          Services
+        </a>
       </nav>
+
       <button className="bg-gradient-to-r from-blue-400 to-green-400 px-4 py-2 rounded-full text-white hover:opacity-90">
-        Contact
+      <Link href="/contactUs">Contact Us</Link>
       </button>
     </header>
   );
--- app/contactUs/page.jsx
+++ app/contactUs/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ContactUs from '../components/ContactUs'
+import Header from '../components/Header'
 
 const page = () => {
   return (
     <main>
+      <Header />
       <ContactUs />
     </main>
   )
